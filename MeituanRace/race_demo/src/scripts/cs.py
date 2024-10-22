bed92a19f337b22c4e2c8cf385b00c01a2edb834
tankkkJiang <jiangzhlan@mail2.sysu.edu.cn>
2024-10-22 12:26:23 +0800

    确认文件提交

--- MeituanRace/race_demo/src/scripts/cs.py
+++ MeituanRace/race_demo/src/scripts/cs.py
@@ -686,7 +686,7 @@ class DemoPipeline:
             for it in iterators[:]:
                 try:
                     # 尝试从当前迭代器中提取一个订单
-                    print(f"看看当前事件是啥{self.events}")
+                    print(f"tankkk看看当前事件是啥{self.events}")
                     waybill = next(it)
                     print("提取订单")
                     # 初始化ros变量
