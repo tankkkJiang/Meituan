fa66426dd75594b1f779f93b40ad6b3e6e0a4d3d
tankkkJiang <jiangzhlan@mail2.sysu.edu.cn>
2024-10-23 23:17:41 +0800

    更改restart_container

--- MeituanRace/restart_container copy.sh
+++ MeituanRace/restart_container copy.sh
@@ -1,8 +1,8 @@
 # 目录
-cd MeituanRace
+cd MeituanRace/Meituan/MeituanRace
 # 拉取代码- GitHub账号配置好的 重新取名 教程
 git fetch origin
-git reset --hard origin/master
+git reset --hard origin/main
 # 原来的容器关掉
 ./stop_race.sh
 # 重新启动容器
@@ -20,6 +20,8 @@ exit
 docker cp race_demo/src/scripts/ race_user_sdk_container:/root/catkin_ws/src/race_demo/src/
 docker exec -it race_user_sdk_container /bin/bash 
 
+# 修改文件权限
+chmod +x /root/catkin_ws/src/race_demo/src/scripts/cs.py
 cp /home/sdk_for_user/map_client_sdk/for_py/* /root/catkin_ws/src/race_demo/src/scripts/
 cp /home/sdk_for_user/msg/* /root/catkin_ws/src/race_demo/msg/
 cp /home/sdk_for_user/map_client_sdk/for_py/voxel_map_final.bin /root/catkin_ws/
