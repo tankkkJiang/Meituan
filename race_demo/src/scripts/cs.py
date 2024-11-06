dedee59e586fdd8b1d5430112906db7052d286b9
tankkkJiang <jiangzhlan@mail2.sysu.edu.cn>
2024-11-06 20:41:00 +0800

    尝试激进派

--- race_demo/src/scripts/cs.py
+++ race_demo/src/scripts/cs.py
@@ -599,8 +599,8 @@ class DemoPipeline:
                 # 检查小车是否处于运动状态
                 while True:
                     timeout += 1
-                    if timeout > 30:
-                        print("超过15s没有移动，重启循环点移动")
+                    if timeout > 6:
+                        print("超过3s没有移动，重启循环点移动")
                         self.move_car_to_target_pos(car_list)
                         timeout = -20
                     car_physical_status = next(
@@ -906,7 +906,7 @@ class DemoPipeline:
                         bind_cargo_attempts = 0  # 用于跟踪绑定货物的尝试次数
 
                         select_start_time_ms = int(rospy.get_time() * 1000) - self.running_start_time_ms
-                        if self.waybill_count_start > 1 and (select_start_time_ms > (waybill['orderTime'] + 20000)) and ((select_start_time_ms + 15000 > (waybill['timeout'])) or (select_start_time_ms + 135000 > ((waybill['betterTime'] + waybill['timeout']))/2)):
+                        if self.waybill_count_start > 1 and (select_start_time_ms > (waybill['orderTime'] + 80000)) and ((select_start_time_ms + 15000 > (waybill['timeout'])) or (select_start_time_ms + 135000 > ((waybill['timeout'] - waybill['betterTime'])/8)+waybill['betterTime']) or (select_start_time_ms + 125000 > waybill['betterTime'])):
                             # 丢弃这一单，直接开始下一单
                             # 需要满足条件：比ordertime大于6s，如果小于6s有可能挂不上单
                             self.loss_waybill += 1
